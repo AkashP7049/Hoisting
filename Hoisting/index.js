85886596c8effb777221fadf91b4f898eac81d7d
AkashP7049 <akash7049695103gmail.com>
2024-09-13 12:32:20 +0530

    New

--- Hoisting/index.js
+++ Hoisting/index.js
@@ -62,3 +62,67 @@
 //     console.log(a); //10
 // }
 // console.log(a);   //5
+
+
+
+
+//HIGHER ORDER FUNCTION
+  
+
+// function outer(a){
+//     console.log(a);
+//     a()
+// }
+// outer (function inner(){
+//     console.log('hello mai inner hu');
+// })
+
+
+// function outer(){
+//     console.log('outer');
+//     function inner(){
+//         console.log('inner');
+        
+//     }
+//     return inner
+    
+// }
+// let inner=outer
+// inner()
+
+//QUS :LET arr  [ 1,2,3,4,"hello","hii", false]
+
+let arr = [1,2,3,4,'hello','hey',false]
+function number (arr){
+let result =[]
+for (let i of arr){
+    if(typeof i==='number'){
+        result.push(i)
+    }
+}
+return result
+}
+console.log(number(arr));
+
+function element (arr){
+    let result1 =[]
+    for(let i of arr){
+        if(typeof i==='string'){
+            result1.push(i)
+        }
+    }
+    return result1
+}
+console.log(element(arr));
+
+
+function bool (arr){
+    let result1 =[]
+    for(let i of arr){
+        if(typeof i==='boolean'){
+            result1.push(i)
+        }
+    }
+    return result1
+}
+console.log(bool(arr));
