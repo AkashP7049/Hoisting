8daeb24246ac13b1b3aa30f172eabca70b09b192
AkashP7049 <akash7049695103gmail.com>
2024-09-18 14:07:03 +0530

    promises

--- .vscode/lec-4_Promises/index.js
+++ .vscode/lec-4_Promises/index.js
@@ -48,7 +48,8 @@
 //     if(reason==='barish'){
 //         rej()
 //     }else{
-//         res()
+//    
+//     res()
 //     }
  
 // } )
@@ -61,34 +62,34 @@
 
 // promises
 
-let step1=function(){
-    return new Promise((res, )=>{
-        setTimeout(()=>{
-            res('photo select ho gyiii');
-        }, 5000)
-    })
-}
-let step2=function(){
-    return new Promise(( res , )=>{
-        setTimeout(()=>{
-            res('filtereddd')
-        },4000)
-    })
-}
-let step3=function(){
-    return new Promise(( res ,)=>{
-        setTimeout(()=>{
-            res('Captionsssss')
-        },3000)
-    })
-}
-let step4=function(){
-    return new Promise(( res , )=>{
-        setTimeout(()=>{
-            res('post ho gyiiii')
-        },2000)
-    })
-}
+// let step1=function(){
+//     return new Promise((res, )=>{
+//         setTimeout(()=>{
+//             res('photo select ho gyiii');
+//         }, 5000)
+//     })
+// }
+// let step2=function(){
+//     return new Promise(( res , )=>{
+//         setTimeout(()=>{
+//             res('filtereddd')
+//         },4000)
+//     })
+// }
+// let step3=function(){
+//     return new Promise(( res ,)=>{
+//         setTimeout(()=>{
+//             res('Captionsssss')
+//         },3000)
+//     })
+// }
+// let step4=function(){
+//     return new Promise(( res , )=>{
+//         setTimeout(()=>{
+//             res('post ho gyiiii')
+//         },2000)
+//     })
+// }
 // step1().then((data)=>{
 //     console.log(data);
 //     return step2().then((filteddd)=>{
@@ -103,14 +104,101 @@ let step4=function(){
 // })
 
 
-async function magic(){
-    let data =await step1()
-    console.log(data);
-    let data1=await step2()
-    console.log(data1);
-    let data2=await step3()
-    console.log(data2);
-    let data3=await step4()
-    console.log(data3);
+// async function magic(){
+//     let data =await step1()
+//     console.log(data);
+//     let data1=await step2()
+//     console.log(data1);
+//     let data2=await step3()
+//     console.log(data2);
+//     let data3=await step4()
+//     console.log(data3);
+// }
+// magic()
+
+
+
+// function step1(){
+//     return new Promise((res,rej)=>{
+//         setTimeout(()=>{
+//             res('message sent');
+//         },4000)
+//     })
+// }
+
+
+// function step2(){
+//     return new Promise((res,rej)=>{
+//         setTimeout(()=>{
+//             res('message delivered');
+//         },3000)
+//     })
+// }
+
+// function step3(){
+//     return new Promise((res,rej)=>{
+//         setTimeout(()=>{
+//             res('message seen');
+//         },3000)
+//     })
+// }
+// step1().then((data)=>{
+//     console.log(data);
+//     return step2().then((data)=>{
+//         console.log(data);
+//         return step3().then((data)=>{
+//             console.log(data);
+//         })
+//     })
+// })
+
+// async function magic(){
+//     let data =await step1()
+//     console.log(data);
+//     let data1=await step2()
+//     console.log(data1);
+//     let data2=await step3()
+//     console.log(data2);
+
+// }
+// magic()
+
+
+
+function step1(){
+    return new Promise((res,rej)=>{
+        setTimeout(()=>{
+            console.log('Message sent');
+              res()
+        },4000)
+    })
 }
-magic()
+
+
+function step2(){
+    return new Promise((res,rej)=>{
+        setTimeout(()=>{
+            console.log('message delivered');
+            
+            res()
+        },3000)
+    })
+}
+
+function step3(){
+    return new Promise((res,rej)=>{
+        setTimeout(()=>{
+            console.log('message seen');
+            
+            res()
+        },3000)
+    })
+}
+
+async function call() {
+    await step1()
+    await step2()
+    await step3()
+    
+}
+call()
