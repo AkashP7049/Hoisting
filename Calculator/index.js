cc64a63969f2f599e9821f2bd9a65fa113f91877
AkashP7049 <akash7049695103gmail.com>
2024-09-21 15:33:07 +0530

    Calculator

---
+++ Calculator/index.js
@@ -0,0 +1,16 @@
+let inp = document.querySelector('input')
+let button = document.querySelectorAll('button')
+for(let btn of button){
+    btn.addEventListener('click',(e)=>{
+        let text = e.target.innerText
+        if(text==='C'){
+            inp.value=''
+        }
+        else if(text==='='){
+            inp.value=eval(inp.value)
+        }
+        else{
+            inp.value=inp.value+text
+        }
+    })
+}
